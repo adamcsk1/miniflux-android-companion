94bc34642b068a2edeb0698c4b3fa723cd268e1c
adamcsk1 <52232363+adamcsk1@users.noreply.github.com>
2024-08-19 11:00:01 +0200

    update project dependencies

--- app/src/main/java/com/adamcsk1/miniflux_companion/activities/FullscreenActivityBase.kt
+++ app/src/main/java/com/adamcsk1/miniflux_companion/activities/FullscreenActivityBase.kt
@@ -1,6 +1,5 @@
 package com.adamcsk1.miniflux_companion.activities
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.adamcsk1.miniflux_companion.AlertHandler
